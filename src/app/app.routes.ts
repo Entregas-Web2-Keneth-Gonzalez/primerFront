6a5672c36d1886d87a23906dd467694d73af88a9
unknown <kegonzalezca@est.utn.ac.cr>
2024-07-19 20:43:13 -0600

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/components/products/products.component';
+import { LoginComponent } from './pages/components/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:LoginComponent},
+    {path:'products', component: ProductsComponent},
+    {path:'productsDetail', redirectTo:''}
+];
---
+++ src/app/pages/components/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent {
+
+  verDetalle(){
+    alert('Aqui en el detalle del producto')
+  }
+
+}
